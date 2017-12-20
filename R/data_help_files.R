bd4b22f16b6ed60b17fa11c85ed32542ce538e82
theoroe3 <theoroe33@gmail.com>
2017-12-20 15:02:06 +0000

    added data sets from half day course

---
+++ R/data_help_files.R
@@ -0,0 +1,43 @@
+#' @name bond
+#' @title James Bond Information
+#' @description Kills/Alcohol consumed/Nationality of every James Bond
+#' @docType data
+#' @format A data frame
+NULL
+
+#' @name example
+#' @title Example data set
+#' @description Gender, Score, Response, Age
+#' @docType data
+#' @format A data frame
+NULL
+
+#' @name GoTRating
+#' @title Game of Thrones ratings
+#' @description Game of thrones TV ratings for every episode
+#' @docType data
+#' @format A data frame
+NULL
+
+#' @name map
+#' @title World city info
+#' @description Lat/lng  and populations of cities around the world with > 500k population
+#' @docType data
+#' @format A data frame
+NULL
+
+#' @name package_numbers
+#' @title Package numbers over time
+#' @description Total number of packages at each version of R
+#' @docType data
+#' @format A data frame
+NULL
+
+#' @name premier
+#' @title Premier league 2016/2017
+#' @description Information on teams for the 16/17 season, such as summer spend/ net spend (millions)
+#' , final position, % of english players, games til they were safe from relegation, games til they
+#' scored 40 goals.
+#' @docType data
+#' @format A data frame
+NULL
